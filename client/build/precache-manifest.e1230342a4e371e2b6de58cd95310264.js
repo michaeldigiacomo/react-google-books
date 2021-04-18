712aad7290db4b1748e3d9211892b19a46f42fb8
Michael DiGiacomo <mdigiacomo05@me.com>
2021-04-17 20:39:21 -0400

    final commit

---
+++ client/build/precache-manifest.e1230342a4e371e2b6de58cd95310264.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "725090bfd1b68d813eb68fee284decc1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "508e4571a0332627a5ee",
+    "url": "/static/css/main.174b53f3.chunk.css"
+  },
+  {
+    "revision": "b54b35988f847c8fb4de",
+    "url": "/static/js/2.7bf1cf89.chunk.js"
+  },
+  {
+    "revision": "9be8f09f0bfe1d20164d4c42e389b466",
+    "url": "/static/js/2.7bf1cf89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "508e4571a0332627a5ee",
+    "url": "/static/js/main.67050516.chunk.js"
+  },
+  {
+    "revision": "816cf679577cc3650d0c",
+    "url": "/static/js/runtime-main.87c4f50d.js"
+  }
+]);
